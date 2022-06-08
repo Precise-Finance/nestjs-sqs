b3b25090df78d0cd97fecb4f923c53c3794e90e6
Dan Shapir <dan@precise.credit>
2022-06-08 16:20:24 +0300

    Add span to decorator

--- lib/sqs.decorators.ts
+++ lib/sqs.decorators.ts
@@ -1,9 +1,10 @@
-import { SetMetadata } from '@nestjs/common';
+import { applyDecorators, SetMetadata } from '@nestjs/common';
 import { SQS_CONSUMER_EVENT_HANDLER, SQS_CONSUMER_METHOD } from './sqs.constants';
 import { AuditContext } from '@precise/audit';
+import { Span } from '@metinseylan/nestjs-opentelemetry';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const SqsMessageHandler = (name: string, batch?: boolean, auditContext?: Partial<AuditContext>) =>
-  SetMetadata(SQS_CONSUMER_METHOD, { name, batch, auditContext });
+  applyDecorators(Span(), SetMetadata(SQS_CONSUMER_METHOD, { name, batch, auditContext }));
 export const SqsConsumerEventHandler = (name: string, eventName: string) =>
-  SetMetadata(SQS_CONSUMER_EVENT_HANDLER, { name, eventName });
+applyDecorators(Span(), SetMetadata(SQS_CONSUMER_EVENT_HANDLER, { name, eventName }));
