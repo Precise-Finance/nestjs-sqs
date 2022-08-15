a6fa0ed867452c32e40b13feca088fee4323191c
Haim Omesi <haim@precise.credit>
2022-08-15 14:52:26 +0300

    Updated to precise telemtry

--- lib/sqs.decorators.ts
+++ lib/sqs.decorators.ts
@@ -1,10 +1,10 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
 import { SQS_CONSUMER_EVENT_HANDLER, SQS_CONSUMER_METHOD } from './sqs.constants';
 import { AuditContext } from '@precise/audit';
-import { Span } from '@metinseylan/nestjs-opentelemetry';
+import { Span } from '@precise/nestjs-opentelemetry';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const SqsMessageHandler = (name: string, batch?: boolean, auditContext?: Partial<AuditContext>) =>
   applyDecorators(Span(), SetMetadata(SQS_CONSUMER_METHOD, { name, batch, auditContext }));
 export const SqsConsumerEventHandler = (name: string, eventName: string) =>
-applyDecorators(Span(), SetMetadata(SQS_CONSUMER_EVENT_HANDLER, { name, eventName }));
+  applyDecorators(Span(), SetMetadata(SQS_CONSUMER_EVENT_HANDLER, { name, eventName }));
