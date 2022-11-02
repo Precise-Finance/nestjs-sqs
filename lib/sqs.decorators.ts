8b472a144b8226b6be3fface0f56b6bb86d02746
Dan Shapir <dan@precise.credit>
2022-11-02 15:38:22 +0200

    Fix decorator

--- lib/sqs.decorators.ts
+++ lib/sqs.decorators.ts
@@ -5,6 +5,6 @@ import { Span } from '@precise/nestjs-opentelemetry';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const SqsMessageHandler = (name: string, batch?: boolean, auditContext?: Partial<AuditContext>, instances = 1) =>
-  applyDecorators(Span(), SetMetadata(SQS_CONSUMER_METHOD, { name, batch, auditContext }));
+  applyDecorators(Span(), SetMetadata(SQS_CONSUMER_METHOD, { name, batch, auditContext, instances }));
 export const SqsConsumerEventHandler = (name: string, eventName: string) =>
   applyDecorators(Span(), SetMetadata(SQS_CONSUMER_EVENT_HANDLER, { name, eventName }));
